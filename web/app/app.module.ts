e4f879c27d25708e66687e030942c6cc729ac5ae
InkInn <lrc19931109@163.com>
2017-04-12 11:47:09 +0800

    head

--- web/app/app.module.ts
+++ web/app/app.module.ts
@@ -7,7 +7,8 @@ import { CommonModule } from '@angular/common';
 import { PaginatorModule } from 'primeng/primeng';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './appRouting.module';
-import {NavComponent} from "./component/nav/nav.component";
+import { NavComponent } from "./component/nav/nav.component";
+import { HeadComponent } from "./component/head/head.component";
 import {TopComponent} from "./component/top/top.component";
 import {AttractComponent} from "./component/attraction/attraction.component";
 import {NotesComponent} from "./component/notes/notes.component";
@@ -16,13 +17,14 @@ import {ConstantService} from "./service/constant.service";
 import {AttractionService} from "./service/attraction.service";
 import {NoteService} from "./service/note.service";
 import {CookieService} from "angular2-cookie/core";
-import {AppResolver} from "./app.resolver";
+import { AppResolver } from "./app.resolver";
+import { TabViewModule } from 'primeng/primeng';
 
 
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpModule,PaginatorModule,CommonModule,AppRoutingModule],
-    declarations: [AppComponent,AttractComponent,NotesComponent,NavComponent,TopComponent],
+    imports: [BrowserModule, FormsModule, HttpModule, PaginatorModule, CommonModule, AppRoutingModule, TabViewModule],
+    declarations: [AppComponent, AttractComponent, NotesComponent, NavComponent, TopComponent, HeadComponent],
     providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy},NoteService,AttractionService,AreaService,CookieService,ConstantService,AppResolver],
     bootstrap: [AppComponent]
 })
--- web/app/component/head/head.component.ts
+++ web/app/component/head/head.component.ts
@@ -1,23 +1,60 @@
 
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Block } from "../../model/model";
-import { BLOCK } from "../../mock/block.mock";
+import { User } from "../../model/model";
+import { ConstantService } from "../../service/constant.service";
+import { TabViewModule } from 'primeng/primeng';
 
 @Component({
-    selector: 'nav',
+    selector: 'head',
     templateUrl: 'app/component/head/head.component.html',
     styleUrls: [
         "public/common/css/head.css"
     ]
 }) export class HeadComponent implements OnInit {
 
-    constructor(private route: ActivatedRoute, private router: Router) { }
+    constructor(private route: ActivatedRoute,
+                private constantService: ConstantService,
+                private router: Router) { }
+
+    //用户状态  当前是否登登录
+    private isLogin: boolean = false;
+
+    //当前登录用户
+    private currentUser: User;
+
+    //注册用户
+    private registerUser: User;
 
 
 
     //初始化加载
     ngOnInit(): void {
         // }
-    }    
+    }  
+    
+    //获取当前用户
+    getCurrentUser(): void {
+        this.currentUser = this.constantService.getCurrentUser();
+        //当前用户未登录
+        if (this.currentUser == null) {
+            this.isLogin = false;
+        } else {
+            //当前有用户登录
+            this.isLogin = true;
+        }
+    }
+
+
+
+
+    //登录提交
+    login(): void{
+        
+    }
+    //注册提交
+    register(): void{
+        
+    }
+
 }
